f455370233e031cb891211a506c5bac8f06a0039
Roberto Giorgi <giorgi@dii.unisi.it>
2025-11-01 09:45:31 +0100

    updating paths in printout.sh

--- printout.sh
+++ printout.sh
@@ -4,13 +4,14 @@ if [ "$1" = "" ]; then
    echo "ERROR: you must specify a test date or exercise (e.g., 070628 or ex1)"
    exit -1
 fi
-if [ ! -s "./run-$1.sh" ]; then
-   echo "ERROR: cannot find file ./run-$1.sh"
+if [ ! -s "./test/run-$1.sh" ]; then
+   echo "ERROR: cannot find file ./test/run-$1.sh"
    exit -1
 fi
 
-./run-$1.sh -int no > out-$1.txt
-enscript -o out-$1.ps -r -f Courier6 out-$1.txt
+./test/run-$1.sh -int no > out-$1.txt
+#enscript -o out-$1.ps -r -f Courier6 out-$1.txt
+a2ps -R out-$1.txt --columns 1 -o out-$1.ps -f 6
 ps2pdf out-$1.ps
 rm -f out-$1.ps
 echo "--> OUTPUT IN FILE: out-$1.pdf"
